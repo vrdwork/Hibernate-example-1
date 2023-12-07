aa30872d3d61026349941421303db13b1e9786ff
vrdboy <vrdboy26@gmail.com>
2023-12-07 13:23:49 +0100

    Se modifican esquemas de creación

--- spring-boot-projects/hibernate-examples/src/test/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO TBL_EMPLOYEE
-    (first_name, last_name, email)
-VALUES
-    ('Lokesh', 'Gupta', 'abc@gmail.com'),
-    ('Deja', 'Vu', 'xyz@email.com'),
-    ('Caption', 'America', 'cap@marvel.com');
